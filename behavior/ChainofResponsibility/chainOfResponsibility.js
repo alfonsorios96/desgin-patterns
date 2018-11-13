a98d6b84007906363aee3fbfc8576e32a0ff0e9d
braincaus <braincaus@gmail.com>
2018-11-12 19:03:10 -0600

    Cambios

---
+++ behavior/ChainofResponsibility/chainOfResponsibility.js
@@ -0,0 +1,17 @@
+class Request{
+    constructor(amount) {
+        this.amount = amount;
+        console.log("Requested: $" + this.amount)
+    }
+
+    get(bill) {
+        let count = Math.floor(this.amount / bill);
+        this.amount -= count * bill;
+        console.log("Dispense " + count + " $" + bill + " bills");
+        return this;
+    }
+}
+
+let request = new Request(378);
+
+request.get(100).get(50).get(20).get(10).get(5).get(1);
